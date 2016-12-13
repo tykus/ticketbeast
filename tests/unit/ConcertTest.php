6de7fadea010b73843c14d9601c36f623bcefcad
Brian O'Sullivan <brian.osullivan@live.com>
2016-12-13 12:40:01 +0000

    Adding a test and implementation for ticket price in dollars

--- tests/unit/ConcertTest.php
+++ tests/unit/ConcertTest.php
@@ -13,10 +13,30 @@ class ConcertTest extends TestCase
   /** @test */
   public function can_get_formatted_date()
   {
-    $concert = factory(Concert::class)->create([
+    $concert = factory(Concert::class)->make([
       'date' => Carbon::parse('2016-12-01 8:00pm')
     ]);
 
     $this->assertEquals('December 1, 2016', $concert->formatted_date);
+  }
+
+  /** @test */
+  public function can_get_formatted_time()
+  {
+    $concert = factory(Concert::class)->make([
+      'date' => Carbon::parse('2016-12-01 17:00:00')
+    ]);
+
+    $this->assertEquals('5:00pm', $concert->formatted_start_time);
+  }
+
+  /** @test */
+  public function can_get_ticket_price_in_dollars()
+  {
+    $concert = factory(Concert::class)->make([
+      'ticket_price' => 6750
+    ]);
+
+    $this->assertEquals(67.50, $concert->ticket_price_in_dollars);
   }
 }
